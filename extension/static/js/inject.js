19b487d79eb0aaf30461dbbc51197e97f61d424e
Sergey Lubeckiy <s.lubeckiy@gmail.com>
2024-02-02 11:39:29 +0300

    refactor: inject interceptor.js in head

--- extension/static/js/inject.js
+++ extension/static/js/inject.js
@@ -1,52 +1,9 @@
-//   const interceptor = chrome.runtime.getURL("interceptor.js");
-//   const script = document.createElement('script');
-// //   const textnode = document.createTextNode(interceptor);
-// //   script.appendChild(textnode);
-//   script.src = interceptor;
-//   document.documentElement.append(script);
-//   script.onload = function(){
-//     console.log('Script interceptor.js injected.')
-//   }
-
-//   chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-//     console.log(tabs[0].url);
-//     console.log(tabs[0].id);
-//     my_tabid=tabs[0].id;
-// }); 
-// alert(my_tabid);
-
-// chrome.scripting.executeScript(
-// {
-//     target: {tabId: my_tabid},
-//     files: ['interceptor.js'],
-// });
-
-/*
- * Copyright © 2022 EPAM Systems, Inc. All Rights Reserved. All information contained herein is, and remains the
- * property of EPAM Systems, Inc. and/or its suppliers and is protected by international intellectual
- * property law. Dissemination of this information or reproduction of this material is strictly forbidden,
- * unless prior written permission is obtained from EPAM Systems, Inc
- */
-
-const nullthrows = (v) => {
-  if (v == null) throw new Error("it's a null");
-  return v;
-}
-
-function injectCode(src) {
+document.addEventListener("DOMContentLoaded", function() {
   const script = document.createElement('script');
-  // This is why it works!
   script.type = "text/javascript";
-  script.src = src;
+  script.src = chrome.runtime.getURL("/static/js/interceptor.js");
   script.onload = function () {
-        console.log("script injected");
-        //this.remove();
+    console.log("script injected");
   };
-
-  // This script runs before the <head> element is created,
-  // so we add the script to <html> instead.
-  nullthrows(document.head || document.documentElement).appendChild(script);
-}
-
-
-injectCode(chrome.runtime.getURL("/static/js/interceptor.js"));
+  document.head.appendChild(script)
+});
